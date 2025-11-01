798957477e65ddf0227f3bdc2dacdb9d2c07eb6d
bquang016 <buidangquangmtp@gmail.com>
2025-11-01 11:21:09 +0700

    first commit

---
+++ src/main/java/com/example/smart_booking_system/SmartBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartBookingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smart_booking_system/SmartBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smart_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
