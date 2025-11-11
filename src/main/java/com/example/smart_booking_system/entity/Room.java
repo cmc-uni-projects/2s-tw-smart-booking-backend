2f0c00fc8bd23d3ae081be65d29e21389eab69f0
vthai <vietthai22825@outlook.com>
2025-11-11 23:04:22 +0700

    Initializer room

---
+++ src/main/java/com/example/smart_booking_system/entity/Room.java
@@ -0,0 +1,29 @@
+package com.example.smart_booking_system.entity;
+
+import com.example.smart_booking_system.enums.RoomCategory;
+import com.example.smart_booking_system.enums.RoomStatus;
+import com.mysql.cj.protocol.ColumnDefinition;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int roomId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "propertyId")
+    private Property propertyId;
+    @Enumerated(EnumType.STRING)
+    private RoomCategory roomCategory;
+    private String roomName;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private int capacity;
+    private BigDecimal pricePerNight;
+    @Enumerated(EnumType.STRING)
+    private RoomStatus roomStatus;
+    private boolean isActive;
+}
---
+++ src/main/java/com/example/smart_booking_system/enums/RoomCategory.java
@@ -0,0 +1,7 @@
+package com.example.smart_booking_system.enums;
+
+public enum RoomCategory {
+    STANDARD,
+    DELUXE,
+    SUITE
+}
