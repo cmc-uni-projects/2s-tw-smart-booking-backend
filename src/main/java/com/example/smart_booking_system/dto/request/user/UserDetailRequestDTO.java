02f5c656568c858cdb3003c540e7d4b545d4369d
bquang016 <buidangquangmtp@gmail.com>
2025-11-14 02:25:20 +0700

    kết nối api thành công - thông tin cá nhân

--- src/main/java/com/example/smart_booking_system/dto/request/user/UserDetailRequestDTO.java
+++ src/main/java/com/example/smart_booking_system/dto/request/user/UserDetailRequestDTO.java
@@ -1,10 +1,17 @@
 package com.example.smart_booking_system.dto.request.user;
 
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserDetailRequestDTO {
+
+    @Size(min = 2, max = 255, message = "Họ và tên phải từ 2 đến 255 ký tự")
+    private String fullName; // Thêm trường này
+
+    @Pattern(regexp = "(84|0[3|5|7|8|9])+([0-9]{8})\\b", message = "Số điện thoại không hợp lệ")
+    private String phoneNumber; // Thêm trường này
 
     @Size(max = 50, message = "Giới tính phải ít hơn 50 ký tự")
     private String gender;
