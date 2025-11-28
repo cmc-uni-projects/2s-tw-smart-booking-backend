a932ea336138f5bf4f371f476c4239f0b475bc40
Quang. Bui Dang - CMC Global TTS <buidangquangmtp@gmail.com>
2025-11-28 10:36:33 +0700

    phân hạng thành viên

---
+++ src/main/java/com/example/smart_booking_system/enums/MembershipRank.java
@@ -0,0 +1,8 @@
+package com.example.smart_booking_system.enums;
+
+public enum MembershipRank {
+    BRONZE,   // Đồng (Mặc định)
+    SILVER,   // Bạc (> 1000)
+    GOLD,     // Vàng (> 5000)
+    DIAMOND   // Kim cương (> 10000)
+}
